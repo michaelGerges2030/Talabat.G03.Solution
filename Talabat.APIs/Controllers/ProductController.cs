31e70878740c168183481ac1232788d502c7acb8
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-11 21:04:20 +0200

    Git Products Endpoint

--- Talabat.APIs/Controllers/ProductController.cs
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -13,5 +13,13 @@ namespace Talabat.APIs.Controllers
         {
 			_productsRepo = productsRepo;
 		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			var products = await _productsRepo.GetAllAsync();
+
+			return Ok(products);	
+		}
     }
 }
--- Talabat.Repository/GenericRepository.cs
+++ Talabat.Repository/GenericRepository.cs
@@ -21,6 +21,8 @@ namespace Talabat.Repository
 
         public async Task<IEnumerable<T>> GetAllAsync()
 		{
+			if (typeof(T) == typeof(Product))
+				return (IEnumerable<T>) await _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync();
 			return await _dbContext.Set<T>().ToListAsync();	
 		}
 
