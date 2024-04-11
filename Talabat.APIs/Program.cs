6b5acf148a7e12c9ec476b5517e0df29e46b915b
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-11 15:05:33 +0200

    Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,8 +5,9 @@ namespace Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
+
 			var builder = WebApplication.CreateBuilder(args);
 
 			#region Configure Services
@@ -26,6 +27,24 @@ namespace Talabat.APIs
 			#endregion
 
 			var app = builder.Build();
+
+			using var scope = app.Services.CreateScope();
+
+		    var services = scope.ServiceProvider;
+		    var _dbContext = services.GetRequiredService<StoreContext>();
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+			try
+			{
+				await _dbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "An Error Has Been Occured During Applying The Migration");
+			}
+		
 
 			#region Configure Kestrel Middlewares
 			
