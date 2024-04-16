5a0885fe0f104a274aa71db954acc7e15313917f
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-17 00:48:06 +0200

    Employee Example - Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications.Employee_Specs;
+using Talabat.Core.Specifications.Product_Specs;
+
+namespace Talabat.APIs.Controllers
+{
+	//Draft example to see what specifications design pattern do and how it's important
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		private readonly IGenericRepository<Employee> _employeeRepo;
+
+		public EmployeeController(IGenericRepository<Employee> employeeRepo)
+        {
+			_employeeRepo = employeeRepo;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+		{
+			var spec = new EmployeeWithDepartmentSpecifications();	
+			var employees = await _employeeRepo.GetAllWithSpecAsync(spec);
+
+			return Ok(employees);
+		}
+
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecifications(id);
+			var employees = await _employeeRepo.GetAllWithSpecAsync(spec);
+
+			if(employees is null) 
+				return NotFound();	
+
+			return Ok(employees);
+		}
+	}
+}
---
+++ Talabat.Core/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	//Draft example to see what specifications design pattern do and how it's important
+	public class Department: BaseEntity
+	{
+        public string Name { get; set; }
+        public DateOnly DateOfCreation { get; set; }
+    }
+}
