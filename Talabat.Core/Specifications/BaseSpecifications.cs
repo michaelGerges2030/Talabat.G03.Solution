0605407980b2659f2321567de0e09d67786c19b6
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-16 22:22:45 +0200

    Refactor GetProducts Using Specification DP

--- Talabat.Core/Specifications/BaseSpecifications.cs
+++ Talabat.Core/Specifications/BaseSpecifications.cs
@@ -10,17 +10,19 @@ namespace Talabat.Core.Specifications
 {
 	public class BaseSpecifications<T> : ISpecifications<T> where T : BaseEntity
 	{
+		public BaseSpecifications()
+		{
+
+		}
+
+		public BaseSpecifications(Expression<Func<T, bool>> criteriaExpression)
+		{
+			Criteria = criteriaExpression;
+		}
+
 		public Expression<Func<T, bool>>? Criteria { get; set; } = null;
 		public List<Expression<Func<T, object>>> Includes { get; set; } = new List<Expression<Func<T, object>>>();
 
-        public BaseSpecifications()
-        {
-            
-        }
 
-        public BaseSpecifications(Expression<Func<T, bool>> criteriaExpression)
-        {
-            Criteria = criteriaExpression;
-        }
     }
 }
