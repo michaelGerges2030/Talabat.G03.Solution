dcb91268bafbe30fcfcfe4f5cb3610cddda41f81
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-17 02:27:35 +0200

    Product DTO Using AutoMapper

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Talabat.APIs.Helpers;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
 using Talabat.Repository;
@@ -32,6 +33,9 @@ namespace Talabat.APIs
 			//builder.Services.AddScoped<IGenericRepository<ProductCategory>, GenericRepository<ProductCategory>>();
 
 			builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+			//builder.Services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
+			builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 			#endregion
 
