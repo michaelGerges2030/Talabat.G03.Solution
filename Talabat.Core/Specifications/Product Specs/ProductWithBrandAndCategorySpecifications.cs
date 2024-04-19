16e5bc132a2474f4e4ace926e34344dee798c09c
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-20 01:29:51 +0200

    Filteration

--- Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
@@ -9,7 +9,11 @@ namespace Talabat.Core.Specifications.Product_Specs
 {
 	public class ProductWithBrandAndCategorySpecifications: BaseSpecifications<Product>
 	{
-        public ProductWithBrandAndCategorySpecifications(string sort):base()
+        public ProductWithBrandAndCategorySpecifications(string? sort, int? brandId, int? categoryId)
+			:base( P =>
+			                   (!brandId.HasValue || P.BrandId == brandId.Value) &&
+			                   (!categoryId.HasValue || P.CategoryId == categoryId.Value)
+			)
         {
 			AddIncludes();
 
