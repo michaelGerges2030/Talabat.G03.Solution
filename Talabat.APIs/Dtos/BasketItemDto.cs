fcfac51fb9e9080b4722b55c2e5a1eebc227aa28
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-23 14:19:42 +0200

    Validate Customer Basket

---
+++ Talabat.APIs/Dtos/BasketItemDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabat.APIs.Dtos
+{
+	public class BasketItemDto
+	{
+		[Required]
+		public int Id { get; set; }
+		
+		[Required]
+		public string ProductName { get; set; }
+
+		[Required]
+		public string PictureUrl { get; set; }
+
+		[Required]
+		[Range(0.1, double.MaxValue, ErrorMessage = "Price Must Be Greater Than zero!")]
+		public decimal Price { get; set; }
+
+		[Required]
+		public string Category { get; set; }
+
+		[Required]
+		public string Brand { get; set; }
+
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Quantity Must Be At Least One Item!")]
+		public int Quantity { get; set; }
+	}
+}
--- Talabat.Core/Entities/BasketItem.cs
+++ Talabat.Core/Entities/BasketItem.cs
@@ -14,7 +14,5 @@
 
 		public int Quantity { get; set; }
 
-       
-
     }
 }
